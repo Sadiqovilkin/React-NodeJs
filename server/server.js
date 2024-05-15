f4f6681bfc57c5206567ee2c760fafd9526017b5
Sadiqovilkin <sadiqovilkin068@gmail.com>
2024-05-15 13:25:11 +0400

    #Node js Added mongoDb

--- server/server.js
+++ server/server.js
@@ -1,45 +1,172 @@
-const express=require("express")
-const app=express()
-const PORT=7070
-const cors= require("cors")
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const dotenv = require('dotenv')
+dotenv.config();
+const Schema = mongoose.Schema;
 
-const data = [
-    {
-        id:"1",
-        title:"Phone"
-    },
-    {
-        id:"2",
-        title:"Computer"
-    },
-    {
-        id:"3",
-        title:"watch"
-    },
-    {
-        id:"4",
-        title:"neckles"
-    },
-]
+app.use(cors())
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended:true}))
+const PORT = 6060
+DB_URI = "mongodb+srv://Admin:Admin123@cluster0.ao8agug.mongodb.net/"
 
-app.get('/api/data', (req, res) => {
-    const {title} = req.query
-    if(title){
-        let filteredData =  data.filter((x)=> x.title.toLowerCase().trim().includes(title.toLowerCase().trim()))
-        res.send(filteredData)
+
+
+
+const UserSchema = new Schema({
+    username: String, 
+    password: String,
+    email: String, 
+    profileImg: String, 
+    balance: Number, 
+    role: String, 
+    basketItems: Array
+},{timestamps:true});
+
+const MessageSchema = new mongoose.Schema({
+    fullName:String, 
+    email:String, 
+    title:String, 
+    message:String
+},{timestamps:true});
+
+const OrderSchema = new mongoose.Schema({
+    userId:String, 
+    totalPrice:String, 
+    status:String, 
+    items:Array 
+},{timestamps:true});
+
+const CategorySchema = new mongoose.Schema({
+    name: String, 
+    
+},{timestamps:true});
+
+const ProductSchema = new mongoose.Schema({
+    name: String, 
+    salePrice:Number, 
+    costPrice:Number, 
+    imgSrc:String, 
+    discountPercentage:Number, 
+    description:String, 
+    categoryId:String, 
+    stock:Number
+    
+},{timestamps:true});
+
+
+const UserModel = mongoose.model('Users', UserSchema);
+const MessageModel = mongoose.model('Messages', MessageSchema);
+const OrderModel = mongoose.model('Orders', OrderSchema);
+const CategoryModel = mongoose.model('Categorys', CategorySchema);
+const ProductModel = mongoose.model('Product', ProductSchema);
+
+// User Reguest
+app.get("/api/users", async (req, res) => {
+    const users = await UserModel.find();
+  
+    if (users.length > 0) {
+      res.status(200).send({
+        message: "success",
+        data: users,
+      });
+    } else {
+      res.send({
+        message: "not found",
+        data: null,
+      });
     }
-    else{
-        res.send(data)
+  });
 
+app.post("/api/users", async (req, res) => {
+    console.log(req.body);
+    const tag = new UserModel(req.body);
+    await tag.save();
+    res.send({
+      message: "posted",
+      data: tag,
+    });
+  });
+  app.delete("/api/users/:id", async (req, res) => {
+    const { id } = req.params;
+    let response;
+    try {
+      response = await UserModel.findByIdAndDelete(id);
+    } catch (error) {
+      res.send({
+        error: error,
+      });
     }
-  })
-  app.get('/api/data/:id', (req, res) => {
-    const {id}= req.params
-    console.log(id)
-    let found = data.find((x)=>x.id == id)
+    res.send({
+      message: "deleted",
+      response: response,
+    });
+  });
+  app.patch("/api/users/:id", async (req, res) => {
+    const { id } = req.params;
+    const response = await UserModel.findByIdAndUpdate(id, req.body);
+    res.send({
+      message: "updated",
+      response: response,
+    });
+  });
+//   -----------------------------------------------------------------------------------------
+// Products Reguest
+app.get("/api/products", async (req, res) => {
+    const users = await ProductModel.find();
+  
+    if (users.length > 0) {
+      res.status(200).send({
+        message: "success",
+        data: users,
+      });
+    } else {
+      res.send({
+        message: "not found",
+        data: null,
+      });
+    }
+  });
 
-    res.send(found)
-  })
-app.listen(PORT,()=>{
+app.post("/api/products", async (req, res) => {
+    const tag = new ProductModel(req.body);
+    await tag.save();
+    res.send({
+      message: "posted",
+      data: tag,
+    });
+  });
+  app.delete("/api/products/:id", async (req, res) => {
+    const { id } = req.params;
+    let response;
+    try {
+      response = await ProductModel.findByIdAndDelete(id);
+    } catch (error) {
+      res.send({
+        error: error,
+      });
+    }
+    res.send({
+      message: "deleted",
+      response: response,
+    });
+  });
+  app.patch("/api/products/:id", async (req, res) => {
+    const { id } = req.params;
+    const response = await ProductModel.findByIdAndUpdate(id, req.body);
+    res.send({
+      message: "updated",
+      response: response,
+    });
+  });
+
+  mongoose.connect(DB_URI).then(() => console.log("Connected!"))
+.catch((err) => {
+  console.log(err);
+});
+app.listen(PORT, () => {
     console.log(PORT)
 })
